3ec3870539fc05f036559a8afe11888ebee59b17
yiyi1 <1061750658@qq.com>
2018-05-31 14:19:11 +0800

    aa

---
+++ y/src/test/java/first/Two.java
@@ -0,0 +1,12 @@
+package first;
+
+import org.testng.annotations.Test;
+
+public class Two {
+	
+	@Test
+	  public void f() {
+		  System.out.println("world");
+	  }
+
+}
---
+++ y/src/test/java/jiekou3/y/Thr.java
@@ -0,0 +1,11 @@
+package jiekou3.y;
+
+import org.testng.annotations.Test;
+
+public class Thr {
+     
+	@Test
+	public void aa() {
+		System.out.println("first");
+	}
+}
